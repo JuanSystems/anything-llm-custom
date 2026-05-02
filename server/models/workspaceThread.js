62ea0fe43e236a8ae84037d44af117c401ab0153
JuanSystems <94084283+JuanSystems@users.noreply.github.com>
2026-05-02 18:34:38 -0500

    chore: change default thread name from 'Thread' to 'Nuevo chat'

--- server/models/workspaceThread.js
+++ server/models/workspaceThread.js
@@ -3,7 +3,7 @@ const slugifyModule = require("slugify");
 const { v4: uuidv4 } = require("uuid");
 
 const WorkspaceThread = {
-  defaultName: "Thread",
+  defaultName: "Nuevo chat",
   writable: ["name"],
 
   /**
